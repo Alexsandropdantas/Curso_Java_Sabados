54766bd7631010c44f351175c59a7140eec8c1ed
Alexsandropdantas <alexsandropdantas@gmail.com>
2024-09-21 16:52:51 -0300

    Primeiros arquivos

---
+++ Fundamentos Java/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int a, b, c;
+        a = 10;
+        b = 20;
+        c = a + b;
+        System.out.println("Valor de a = " + a + "  Valor de b = " + b);
+        System.out.println("Valor de C = " + c);
+
+        c = 2 + 2;
+        System.out.println("Valor de C = " + c);
+
+    }
+}
---
+++ Fundamentos Java/src/ExercicioEscreverComentar.java
@@ -0,0 +1,17 @@
+public class ExercicioEscreverComentar {
+
+    public static void Main(String [] args){ // Inicio
+        
+        System.out.println("Olá Mundo, eu sou Alexsandro");
+
+	// Exemplo de comentário de uma linha
+    
+    /*
+    Exemplo de	   
+    comentário de
+    múltiplas linhas 
+    */
+
+    }
+
+}
